25b114e0ce788dd49ff27ea67b5b079f5ea37286
Riccardo Mazzarini <me@noib3.dev>
2025-11-15 12:53:28 +0100

    feat: make the `nix-jettison` primop return an attrset

--- crates/nix-jettison/src/lib.rs
+++ crates/nix-jettison/src/lib.rs
@@ -3,27 +3,77 @@ use core::ptr;
 
 use nix_bindings_sys::{
     EvalState,
+    Value,
     nix_alloc_primop,
+    nix_alloc_value,
+    nix_bindings_builder_free,
+    nix_bindings_builder_insert,
     nix_c_context,
     nix_c_context_create,
     nix_c_context_free,
     nix_gc_decref,
+    nix_get_int,
     nix_init_int,
+    nix_init_primop,
+    nix_make_attrs,
+    nix_make_bindings_builder,
     nix_register_primop,
-    nix_value,
+    nix_value_decref,
 };
 
 const NO_ARGS: &[*const c_char; 1] = &[ptr::null_mut()];
 
 #[allow(unsafe_op_in_unsafe_fn)]
-unsafe extern "C" fn jettison_lib(
+unsafe extern "C" fn add(
     _user_data: *mut c_void,
     ctx: *mut nix_c_context,
     _state: *mut EvalState,
-    _args: *mut *mut nix_value,
-    ret: *mut nix_value,
+    args: *mut *mut Value,
+    ret: *mut Value,
 ) {
-    nix_init_int(ctx, ret, 0);
+    let a = nix_get_int(ctx, *args.offset(0));
+    let b = nix_get_int(ctx, *args.offset(1));
+    nix_init_int(ctx, ret, a + b);
+}
+
+#[allow(unsafe_op_in_unsafe_fn)]
+unsafe extern "C" fn jettison_lib(
+    _user_data: *mut c_void,
+    ctx: *mut nix_c_context,
+    state: *mut EvalState,
+    _args: *mut *mut Value,
+    ret: *mut Value,
+) {
+    // Create an attrset builder with a capacity of 1.
+    let builder = nix_make_bindings_builder(ctx, state, 1);
+
+    let add_name = c"add".as_ptr();
+
+    let mut add_args: [*const c_char; 3] =
+        [c"a".as_ptr(), c"b".as_ptr(), ptr::null()];
+
+    let add_primop = nix_alloc_primop(
+        ctx,
+        Some(add),
+        2,
+        add_name,
+        add_args.as_mut_ptr(),
+        c"Add two integers together".as_ptr(),
+        ptr::null_mut(),
+    );
+
+    // Convert the primop into a Value and add it to the builder.
+    let add_value = nix_alloc_value(ctx, state);
+    nix_init_primop(ctx, add_value, add_primop);
+    nix_bindings_builder_insert(ctx, builder, add_name, add_value);
+
+    // Finalize the builder.
+    nix_make_attrs(ctx, ret, builder);
+
+    // Clean up.
+    nix_bindings_builder_free(builder);
+    nix_value_decref(ctx, add_value);
+    nix_gc_decref(ctx, add_primop as *const c_void);
 }
 
 #[unsafe(no_mangle)]
