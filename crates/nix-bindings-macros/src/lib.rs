12d6311833cd83465f54e762ae832e7e414d9a1e
Riccardo Mazzarini <me@noib3.dev>
2025-11-20 13:06:12 +0100

    feat: implement the `entry` macro

--- crates/nix-bindings-macros/src/lib.rs
+++ crates/nix-bindings-macros/src/lib.rs
@@ -1,1 +1,14 @@
 //! TODO: docs.
+
+mod entry;
+use proc_macro::TokenStream;
+use syn::parse_macro_input;
+
+/// Marks the entrypoint function of a Nix plugin.
+#[proc_macro_attribute]
+pub fn entry(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as syn::ItemFn);
+    entry::entry(attr, item)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
