48ce54ceaf728697dd05edbe3ac0c087ffc6de07
Riccardo Mazzarini <me@noib3.dev>
2025-11-21 22:03:10 +0100

    refactor: move `FnOnceValue` and `Values` to `value`

--- crates/nix-bindings/src/attrset.rs
+++ crates/nix-bindings/src/attrset.rs
@@ -8,7 +8,7 @@ use nix_bindings_sys as sys;
 
 use crate::namespace::Namespace;
 use crate::prelude::{Context, Result, Utf8CStr, Value, ValueKind};
-use crate::value::AttrsetValue;
+use crate::value::{AttrsetValue, FnOnceValue, Values};
 
 /// TODO: docs.
 pub trait Attrset: Sized {
@@ -87,28 +87,9 @@ pub trait Keys {
 }
 
 /// TODO: docs.
-pub trait Values {
-    /// TODO: docs.
-    const LEN: usize;
-
-    /// TODO: docs.
-    fn with_value<'a, T: 'a>(
-        &'a self,
-        value_idx: usize,
-        fun: impl FnOnceValue<'a, T>,
-    ) -> T;
-}
-
-/// TODO: docs.
 pub trait FnOnceKey<'a, T: 'a> {
     /// TODO: docs.
     fn call(self, value: &'a impl AsRef<Utf8CStr>) -> T;
-}
-
-/// TODO: docs.
-pub trait FnOnceValue<'a, T: 'a> {
-    /// TODO: docs.
-    fn call(self, value: &'a impl Value) -> T;
 }
 
 /// The attribute set type produced by the [`attrset!`](crate::attrset) macro.
@@ -234,11 +215,6 @@ impl<T: Attrset> Attrset for &T {
     }
 }
 
-#[inline(never)]
-fn panic_tuple_index_oob(idx: usize, len: usize) -> ! {
-    panic!("{len}-tuple received out of bounds index: {idx}")
-}
-
 #[rustfmt::skip]
 mod keys_values_impls {
     use super::*;
@@ -280,38 +256,6 @@ mod keys_values_impls {
         };
     }
 
-    macro_rules! impl_values {
-        ($($K:ident),*) => {
-            impl_values!(@pair [] [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31] [$($K)*]);
-        };
-
-        (@pair [$($pairs:tt)*] [$next_idx:tt $($rest_idx:tt)*] [$next_K:ident $($rest_K:ident)*]) => {
-            impl_values!(@pair [$($pairs)* ($next_idx $next_K)] [$($rest_idx)*] [$($rest_K)*]);
-        };
-
-        (@pair [$(($idx:tt $K:ident))*] $_:tt []) => {
-            impl<$($K),*> Values for ($($K,)*)
-            where
-                $($K: Value),*
-            {
-                const LEN: usize = count!($($K)*);
-
-                #[track_caller]
-                #[inline]
-                fn with_value<'a, T: 'a>(
-                    &'a self,
-                    value_idx: usize,
-                    _fun: impl FnOnceValue<'a, T>,
-                ) -> T {
-                    match value_idx {
-                        $($idx => _fun.call(&self.$idx),)*
-                        other => panic_tuple_index_oob(other, <Self as Values>::LEN),
-                    }
-                }
-            }
-        };
-    }
-
     impl_keys!();
     impl_keys!(K);
     impl_keys!(K1, K2);
@@ -330,21 +274,8 @@ mod keys_values_impls {
     impl_keys!(K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15);
     impl_keys!(K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16);
 
-    impl_values!();
-    impl_values!(V);
-    impl_values!(V1, V2);
-    impl_values!(V1, V2, V3);
-    impl_values!(V1, V2, V3, V4);
-    impl_values!(V1, V2, V3, V4, V5);
-    impl_values!(V1, V2, V3, V4, V5, V6);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15);
-    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16);
+    #[inline(never)]
+    fn panic_tuple_index_oob(idx: usize, len: usize) -> ! {
+        panic!("{len}-tuple received out of bounds index: {idx}")
+    }
 }
--- crates/nix-bindings/src/value.rs
+++ crates/nix-bindings/src/value.rs
@@ -17,6 +17,63 @@ use crate::prelude::{
 };
 
 /// TODO: docs.
+pub trait Value: Sized {
+    /// Returns the kind of this value.
+    fn kind(&self) -> ValueKind;
+
+    /// Writes this value into the given, pre-allocated destination.
+    #[doc(hidden)]
+    unsafe fn write(
+        &self,
+        dest: NonNull<sys::Value>,
+        ctx: &mut Context,
+    ) -> Result<()>;
+
+    /// TODO: docs.
+    #[doc(hidden)]
+    #[inline]
+    unsafe fn write_with_namespace(
+        &self,
+        dest: NonNull<sys::Value>,
+        #[expect(unused_variables)] namespace: impl Namespace,
+        ctx: &mut Context,
+    ) -> Result<()> {
+        debug_assert_ne!(self.kind(), ValueKind::Attrset);
+        unsafe { self.write(dest, ctx) }
+    }
+}
+
+/// A trait for types that can be fallibly converted into [`Value`]s.
+pub trait TryIntoValue {
+    /// Attempts to convert this value into a [`Value`].
+    fn try_into_value(
+        self,
+        ctx: &mut Context,
+    ) -> Result<impl Value + use<Self>>;
+}
+
+/// TODO: docs.
+pub trait Values {
+    /// TODO: docs.
+    const LEN: usize;
+
+    /// TODO: docs.
+    fn with_value<'a, T: 'a>(
+        &'a self,
+        value_idx: usize,
+        fun: impl FnOnceValue<'a, T>,
+    ) -> T;
+}
+
+/// A trait to get around the lack of support for generics in closures.
+///
+/// This is semantically equivalent to `FnOnce(&impl Value) -> T`.
+pub trait FnOnceValue<'a, T: 'a> {
+    /// Calls the function with the given value.
+    fn call(self, value: &'a impl Value) -> T;
+}
+
+/// TODO: docs.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum ValueKind {
     /// TODO: docs.
@@ -51,42 +108,6 @@ pub enum ValueKind {
 
     /// TODO: docs.
     Thunk,
-}
-
-/// TODO: docs.
-pub trait Value: Sized {
-    /// Returns the kind of this value.
-    fn kind(&self) -> ValueKind;
-
-    /// Writes this value into the given, pre-allocated destination.
-    #[doc(hidden)]
-    unsafe fn write(
-        &self,
-        dest: NonNull<sys::Value>,
-        ctx: &mut Context,
-    ) -> Result<()>;
-
-    /// TODO: docs.
-    #[doc(hidden)]
-    #[inline]
-    unsafe fn write_with_namespace(
-        &self,
-        dest: NonNull<sys::Value>,
-        #[expect(unused_variables)] namespace: impl Namespace,
-        ctx: &mut Context,
-    ) -> Result<()> {
-        debug_assert_ne!(self.kind(), ValueKind::Attrset);
-        unsafe { self.write(dest, ctx) }
-    }
-}
-
-/// A trait for types that can be fallibly converted into [`Value`]s.
-pub trait TryIntoValue {
-    /// Attempts to convert this value into a [`Value`].
-    fn try_into_value(
-        self,
-        ctx: &mut Context,
-    ) -> Result<impl Value + use<Self>>;
 }
 
 impl Value for () {
@@ -405,5 +426,70 @@ impl<T: TryIntoValue, E: ToError> TryIntoValue for core::result::Result<T, E> {
             Ok(value) => value.try_into_value(ctx),
             Err(err) => Err(ctx.make_error(err)),
         }
+    }
+}
+
+#[rustfmt::skip]
+mod values_impls {
+    use super::*;
+
+    macro_rules! count {
+        () => { 0 };
+        ($x:tt $($xs:tt)*) => { 1 + count!($($xs)*) };
+    }
+
+    macro_rules! impl_values {
+        ($($K:ident),*) => {
+            impl_values!(@pair [] [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31] [$($K)*]);
+        };
+
+        (@pair [$($pairs:tt)*] [$next_idx:tt $($rest_idx:tt)*] [$next_K:ident $($rest_K:ident)*]) => {
+            impl_values!(@pair [$($pairs)* ($next_idx $next_K)] [$($rest_idx)*] [$($rest_K)*]);
+        };
+
+        (@pair [$(($idx:tt $K:ident))*] $_:tt []) => {
+            impl<$($K),*> Values for ($($K,)*)
+            where
+                $($K: Value),*
+            {
+                const LEN: usize = count!($($K)*);
+
+                #[track_caller]
+                #[inline]
+                fn with_value<'a, T: 'a>(
+                    &'a self,
+                    value_idx: usize,
+                    _fun: impl FnOnceValue<'a, T>,
+                ) -> T {
+                    match value_idx {
+                        $($idx => _fun.call(&self.$idx),)*
+                        other => panic_tuple_index_oob(other, <Self as Values>::LEN),
+                    }
+                }
+            }
+        };
+    }
+
+    impl_values!();
+    impl_values!(V);
+    impl_values!(V1, V2);
+    impl_values!(V1, V2, V3);
+    impl_values!(V1, V2, V3, V4);
+    impl_values!(V1, V2, V3, V4, V5);
+    impl_values!(V1, V2, V3, V4, V5, V6);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15);
+    impl_values!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16);
+
+    #[inline(never)]
+    fn panic_tuple_index_oob(idx: usize, len: usize) -> ! {
+        panic!("{len}-tuple received out of bounds index: {idx}")
     }
 }
