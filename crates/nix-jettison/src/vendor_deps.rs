ae72f0fdaf81d97776f4d01c4412053d8c69e90c
Riccardo Mazzarini <me@noib3.dev>
2025-11-29 20:25:26 +0100

    feat: implement `VendorDeps::create_vendor_dir()`

--- crates/nix-jettison/src/vendor_deps.rs
+++ crates/nix-jettison/src/vendor_deps.rs
@@ -19,14 +19,11 @@ pub(crate) struct VendorDepsArgs<'a> {
 }
 
 /// The type of error that can occur when vendoring dependencies fails.
-#[derive(Debug, derive_more::Display)]
+#[derive(Debug, derive_more::Display, cauchy::From)]
 #[display("{_0}")]
 pub(crate) enum VendorDepsError {
-    /// The `fetchGit` function was not found in the provided `pkgs`.
-    MissingFetchGit(NixError),
-
-    /// The `fetchurl` function was not found in the provided `pkgs`.
-    MissingFetchurl(NixError),
+    /// A Nix runtime error occurred.
+    Nix(#[from] NixError),
 
     /// Parsing the contents of the `Cargo.lock` failed.
     #[display("failed to parse Cargo.lock at {path:?}: {err}")]
@@ -64,12 +61,55 @@ pub(crate) enum DependencySource<'lock> {
     Path,
 }
 
+/// The functions that will have to be called to create the vendor directory.
+struct CreateVendorDirFuns<'pkgs> {
+    link_farm: NixFunction<'pkgs>,
+    fetchurl: NixFunction<'pkgs>,
+    fetch_git: NixFunction<'static>,
+}
+
 impl VendorDeps {
-    fn create_vendor_dir(
-        _deps: &[Dependency<'_>],
-        _fetchurl: NixValue<'_>,
-        _fetch_git: NixValue<'_>,
-    ) -> impl Value + use<> {
+    fn create_vendor_dir<'a>(
+        funs: CreateVendorDirFuns<'_>,
+        deps: impl Iterator<Item = Dependency<'a>>,
+        ctx: &mut Context,
+    ) -> Result<Thunk<'static, NixAttrset<'static>>, NixError> {
+        let mut links = Vec::new();
+
+        for dep in deps {
+            let source_path = match dep.source {
+                DependencySource::CratesIo { checksum } => {
+                    let url = format!(
+                        "https://crates.io/api/v1/crates/{name}/{version}/download",
+                        name = dep.name,
+                        version = dep.version,
+                    );
+                    let args = attrset! {
+                        url: url,
+                        sha256: checksum,
+                    };
+                    funs.fetchurl.call::<NixAttrset>(args, ctx)?
+                },
+                DependencySource::Git { url, rev } => {
+                    let args = attrset! {
+                        url: url,
+                        rev: rev,
+                    };
+                    funs.fetch_git.call::<NixAttrset>(args, ctx)?
+                },
+                DependencySource::Path => continue,
+            };
+
+            links.push(attrset! {
+                name: dep.name,
+                path: source_path,
+            });
+        }
+
+        funs.link_farm.call_multi(
+            (c"vendored-deps", links.into_list().into_value()),
+            ctx,
+        )
     }
 
     fn parse_lockfile(
@@ -80,13 +120,26 @@ impl VendorDeps {
     }
 }
 
+impl<'pkgs> CreateVendorDirFuns<'pkgs> {
+    fn new(
+        pkgs: NixAttrset<'pkgs>,
+        ctx: &mut Context,
+    ) -> Result<Self, NixError> {
+        Ok(Self {
+            link_farm: pkgs.get(c"linkFarm", ctx)?,
+            fetchurl: pkgs.get(c"fetchurl", ctx)?,
+            fetch_git: todo!(),
+        })
+    }
+}
+
 impl Function for VendorDeps {
     type Args<'a> = VendorDepsArgs<'a>;
 
     fn call<'a: 'a>(
         args: Self::Args<'a>,
         ctx: &mut Context,
-    ) -> Result<impl Value + use<>, VendorDepsError> {
+    ) -> Result<NixAttrset<'static>, VendorDepsError> {
         let cargo_lock = match fs::read_to_string(&args.cargo_lock) {
             Ok(contents) => contents,
             Err(err) => {
@@ -98,7 +151,7 @@ impl Function for VendorDeps {
         };
 
         let deps = match Self::parse_lockfile(&cargo_lock) {
-            Ok(deps) => deps.collect::<Vec<_>>(),
+            Ok(deps) => deps,
             Err(err) => {
                 return Err(VendorDepsError::ParseCargoLock {
                     path: args.cargo_lock.into_owned(),
@@ -107,17 +160,11 @@ impl Function for VendorDeps {
             },
         };
 
-        let fetchurl = args
-            .pkgs
-            .get(c"fetchurl", ctx)
-            .map_err(VendorDepsError::MissingFetchurl)?;
+        let funs = CreateVendorDirFuns::new(args.pkgs, ctx)?;
 
-        let fetch_git = args
-            .pkgs
-            .get(c"fetchGit", ctx)
-            .map_err(VendorDepsError::MissingFetchGit)?;
-
-        Ok(Self::create_vendor_dir(&deps, fetchurl, fetch_git))
+        Self::create_vendor_dir(funs, deps, ctx)?
+            .force(ctx)
+            .map_err(Into::into)
     }
 }
 
