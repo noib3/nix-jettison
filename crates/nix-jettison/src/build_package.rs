9c1d3c0e70ced3ab1f3900093b1431544a07d3d6
Riccardo Mazzarini <me@noib3.dev>
2025-12-04 09:24:45 +0100

    feat: create Cargo home w/ config that uses vendored sources

--- crates/nix-jettison/src/build_package.rs
+++ crates/nix-jettison/src/build_package.rs
@@ -1,12 +1,14 @@
 use core::ffi::CStr;
 use core::result::Result;
 use std::borrow::Cow;
+use std::env;
 use std::ffi::CString;
 use std::path::Path;
 
+use anyhow::Context as _;
 use cargo::GlobalContext;
-use cargo::core::Workspace;
-use nix_bindings::prelude::*;
+use cargo::core::{Shell, Workspace};
+use nix_bindings::prelude::{Error as NixError, *};
 
 use crate::vendor_deps::{VendorDeps, VendorDepsArgs, VendorDepsError};
 
@@ -25,14 +27,34 @@ pub(crate) struct BuildPackageArgs<'a> {
 #[derive(Debug, derive_more::Display, cauchy::From)]
 #[display("{_0}")]
 pub(crate) enum BuildPackageError {
-    /// Constructing the [`GlobalContext`] failed.
-    CreateContext(anyhow::Error),
-
     /// Constructing the [`Workspace`] failed.
     CreateWorkspace(anyhow::Error),
 
+    /// Getting the current working directory failed..
+    Cwd(anyhow::Error),
+
+    /// A Nix runtime error occurred.
+    Nix(#[from] NixError),
+
     /// Vendoring the dependencies failed.
     VendorDeps(#[from] VendorDepsError),
+}
+
+impl BuildPackage {
+    fn generate_cargo_config(vendor_dir: &Path) -> String {
+        let vendored_sources = "vendored-sources";
+
+        format!(
+            r#"
+[source.crates-io]
+replace-with = "{vendored_sources}"
+
+[source.{vendored_sources}]
+directory = "{}"
+"#,
+            vendor_dir.display()
+        )
+    }
 }
 
 impl Function for BuildPackage {
@@ -47,12 +69,27 @@ impl Function for BuildPackage {
             cargo_lock: args.src.join("Cargo.lock").into(),
         };
 
-        let _vendor_dir = <VendorDeps as Function>::call(vendor_args, ctx)?;
+        let vendor_dir_drv = <VendorDeps as Function>::call(vendor_args, ctx)?;
+        let vendor_dir = vendor_dir_drv.get::<String>(c"outPath", ctx)?;
+        let cargo_config = Self::generate_cargo_config(Path::new(&vendor_dir));
+
+        let cargo_home_drv = args
+            .pkgs
+            .get::<NixLambda>(c"writeTextDir", ctx)?
+            .call_multi::<NixAttrset>(("config.toml", cargo_config), ctx)?
+            .force(ctx)?;
+
+        let cargo_home_path =
+            cargo_home_drv.get::<&Path>(c"outPath", ctx)?.to_owned();
+
+        let cwd = env::current_dir()
+            .context("couldn't get the current directory of the process")
+            .map_err(BuildPackageError::Cwd)?;
+
+        let global_ctx =
+            GlobalContext::new(Shell::new(), cwd, cargo_home_path);
 
         let manifest_path = args.src.join("Cargo.toml");
-
-        let global_ctx = GlobalContext::default()
-            .map_err(BuildPackageError::CreateContext)?;
 
         let workspace = Workspace::new(&manifest_path, &global_ctx)
             .map_err(BuildPackageError::CreateWorkspace)?;
