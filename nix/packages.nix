391d274164e7e66e05278702c53c3375d96f4b88
Riccardo Mazzarini <me@noib3.dev>
2025-12-14 13:56:00 +0100

    feat: add a `bootstrapped` package that builds jettison w/o jettison

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ self, ... }:
 
 {
   imports = [
@@ -16,6 +16,8 @@
     }:
     let
       packageName = (lib.importTOML ../Cargo.toml).package.name;
+      dllName = builtins.replaceStrings [ "-" ] [ "_" ] packageName;
+      dllSuffix = if pkgs.stdenv.isDarwin then "dylib" else "so";
 
       mkPackage =
         {
@@ -26,7 +28,7 @@
           inherit (common) nativeBuildInputs;
           buildInputs = common.mkBuildInputs targetPkgs;
           env = common.mkEnv targetPkgs;
-          jettison = builtins.jettison.buildPackage {
+          jettison = self.lib.buildPackage {
             package = packageName;
             pkgs = targetPkgs;
             src = ../.;
@@ -45,17 +47,54 @@
             inherit release;
             rustc = rust.mkToolchain targetPkgs;
           };
-          dllName = builtins.replaceStrings [ "-" ] [ "_" ] packageName;
-          dllSuffix = if targetPkgs.stdenv.isDarwin then "dylib" else "so";
         in
         pkgs.runCommand "${packageName}${lib.optionalString (!release) "-dev"}" { } ''
           mkdir -p $out
           src=$(readlink -f ${jettison.lib}/lib/lib${dllName}.${dllSuffix})
           cp $src $out/${dllName}.so
         '';
+
+      bootstrapped =
+        let
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust.mkToolchain pkgs;
+            rustc = rust.mkToolchain pkgs;
+          };
+          cargoToml = lib.importTOML ../Cargo.toml;
+          jettison = rustPlatform.buildRustPackage {
+            pname = packageName;
+            version = cargoToml.workspace.package.version;
+            src = lib.fileset.toSource {
+              root = ../.;
+              fileset = lib.fileset.fileFilter (
+                file:
+                (lib.hasSuffix ".rs" file.name)
+                || (lib.hasSuffix ".cpp" file.name)
+                || (file.name == "Cargo.toml")
+                || (file.name == "Cargo.lock")
+              ) ../.;
+            };
+            cargoLock = {
+              lockFile = ../Cargo.lock;
+              outputHashes = {
+                "cauchy-0.1.0" = "sha256-3Z4yHxAnscoysPYPfx9ULMLDS6uaJUkte9IPcnwrbOE=";
+              };
+            };
+            inherit (common) nativeBuildInputs;
+            buildInputs = common.mkBuildInputs pkgs ++ [ pkgs.curl.dev ];
+            env = common.mkEnv pkgs;
+            buildType = "release";
+          };
+        in
+        pkgs.runCommand "${packageName}-bootstrapped" { } ''
+          mkdir -p $out
+          src=$(readlink -f ${jettison}/lib/lib${dllName}.${dllSuffix})
+          cp $src $out/${dllName}.so
+        '';
     in
     {
       packages = {
+        inherit bootstrapped;
         default = mkPackage { release = true; };
         dev = mkPackage { release = false; };
       };
