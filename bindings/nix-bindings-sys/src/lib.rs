f2bea4b93e2b38f92f3345d9b1c432abf3e8f608
Riccardo Mazzarini <me@noib3.dev>
2025-12-14 12:22:58 +0100

    build: make `nix-bindings-sys` `no_std`

--- bindings/nix-bindings-sys/src/lib.rs
+++ bindings/nix-bindings-sys/src/lib.rs
@@ -1,7 +1,8 @@
 #![allow(missing_docs)]
-#![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![no_std]
 
 /// The maximum arity of a primitive operation.
 ///
